e75a18278daa22123bccf593e9c252811c869afc
Sai Potamsetti <Sai.potamsetti@gmail.com>
2017-10-14 14:22:33 -0500

    added bing geocoding and bing image search apis

--- destinymap/js/index.js
+++ destinymap/js/index.js
@@ -1,7 +1,50 @@
 var map; 
+var map_key = 'JZo1Uqz4Z9MlLYP5oPwu~VYDNW7mVzdzzBJDpm3A2xA~AvXLaoIboRB7L-3stqKgzYl0J5YRfaRHQWewd86ikXTNgtCCpFm7FJUVkEX7XQFS';
 
 function loadMapScenario() { 
 	map = new Microsoft.Maps.Map(document.getElementById('myMap'), { 
-		credentials: 'JZo1Uqz4Z9MlLYP5oPwu~VYDNW7mVzdzzBJDpm3A2xA~AvXLaoIboRB7L-3stqKgzYl0J5YRfaRHQWewd86ikXTNgtCCpFm7FJUVkEX7XQFS' 
+		credentials: map_key 
 	}); 
 }
+
+function bingImageSearch(){
+	var params = {
+        "q": "Himachal pradesh must visit spots",
+        "count": "100",
+        "offset": "0",
+        "mkt": "en-us",
+        "safeSearch": "Moderate",
+    };
+
+	var endpoint = "https://api.cognitive.microsoft.com/bing/v7.0/images/search?"+$.param(params);
+
+	$.ajax({
+		url : endpoint,
+		beforeSend: function(xhrObj){
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","5acf393925124e37a57af73df43dfbf6");
+        },
+        type : "GET",
+        data : "" 
+    }).done(function(data) {
+        console.log(data);
+    }).fail(function() {
+        console.log("error");
+    });
+}
+
+function getPosition(){
+	var request = "http://dev.virtualearth.net/REST/v1/Locations?query=triund_himachal_pradesh&key="+map_key;
+	$.ajax({
+		url : request, 
+		data : "",
+		type : "GET",
+		success: function(data){
+			console.log(data);
+		},
+		error: function(err){
+			console.log(err);
+		}
+	});
+}
+getPosition();
+//bingImageSearch();
