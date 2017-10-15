2dcff8bf1e2cb470c1d59c83ac524dabe0b2eec8
Sai Potamsetti <Sai.potamsetti@gmail.com>
2017-10-15 03:21:24 -0500

    checkpoint #1

--- destinymap/js/index.js
+++ destinymap/js/index.js
@@ -1,5 +1,7 @@
 var map; 
 var map_key = 'JZo1Uqz4Z9MlLYP5oPwu~VYDNW7mVzdzzBJDpm3A2xA~AvXLaoIboRB7L-3stqKgzYl0J5YRfaRHQWewd86ikXTNgtCCpFm7FJUVkEX7XQFS';
+var imgs = [];
+var current = 0;
 
 function loadMapScenario() { 
 	map = new Microsoft.Maps.Map(document.getElementById('myMap'), { 
@@ -12,26 +14,82 @@ function loadMapScenario() {
     });
 
     map.setView({
-        center: new Microsoft.Maps.Location(39.601579130, -42.810403254),
+        center: new Microsoft.Maps.Location(36.69938369, -4.421396876),
         zoom: 3
     });
 
+    var infobox = new Microsoft.Maps.Infobox(map.getCenter(), { visible: false });
+    infobox.setMap(map);
+    
     getLocations(function(positions){
         for(var position in positions){
             if(positions.hasOwnProperty(position)){
                 var latitude = positions[position]['latitude']; 
                 var longitude = positions[position]['longitude'];
-                
-                var pushpin = new Microsoft.Maps.Pushpin({'latitude':latitude, 'longitude':longitude}, null);
+                var location = new Microsoft.Maps.Location(latitude, longitude);
+
+                var pushpin = new Microsoft.Maps.Pushpin(location, {title : position, color:'purple'});
+                pushpin.setOptions({ enableHoverStyle: true, enableClickedStyle: true });
+                pushpin.metadata = {
+                    name : position                
+                };
                 map.entities.push(pushpin);
+
+                Microsoft.Maps.Events.addHandler(pushpin, 'click', function(args){
+                    var loc = args.target.getLocation();
+                    var t = args.target.metadata.name;
+                    var imghtml = "";
+                    
+                    imgs = [];
+                    current = 0;
+
+                    bingImageSearch(t, function(data){
+                        console.log(data);
+                        var urls = data['value'];
+            
+                        for(var i = 0; i < urls.length; i++){
+                            imgs.push([urls[i]['contentUrl'], urls[i]['thumbnailUrl'], urls[i]['name']]);
+                        }
+                        
+                        console.log(imgs);
+                        var carousel = "<div class='container'><div class='top' id='title' >"+imgs[current][2]+"</div><div class='top'><div class='left'><img onClick = 'left()' src='https://cdn1.iconfinder.com/data/icons/media-controls/32/circle-play-512.png'/></div><div class='i'><img id='image' src="+imgs[current][1]+" /></div><div class='right'><img onClick='right()' src='https://cdn1.iconfinder.com/data/icons/media-controls/32/circle-play-512.png'/></div></div></div>";
+
+                        infobox.setOptions({
+                            htmlContent : carousel
+                        });
+                    });
+
+                    infobox.setOptions({
+                        location: loc,
+                        title: t,
+                        visible: true,
+                        htmlContent : "<div style='background:white; padding:2%; border:1px solid blue; width:100px;'>"+t+"</div>"
+                    });
+                });
             }
         }
     });
 }
 
-function bingImageSearch(){
+function left(){
+    console.log("left");
+    current--;
+    if(current < 0) current += imgs.length;
+    document.getElementById('image').src = imgs[current][1];
+    document.getElementById('title').innerHTML = imgs[current][2];
+}
+
+function right(){
+    console.log("right");
+    current++;
+    current = current%imgs.length;
+    document.getElementById('image').src = imgs[current][1];
+    document.getElementById('title').innerHTML = imgs[current][2];
+}
+
+function bingImageSearch(q, cb){
 	var params = {
-        "q": "Himachal pradesh must visit spots",
+        "q": q,
         "count": "100",
         "offset": "0",
         "mkt": "en-us",
@@ -48,7 +106,7 @@ function bingImageSearch(){
         type : "GET",
         data : "" 
     }).done(function(data) {
-        console.log(data);
+        cb(data);
     }).fail(function() {
         console.log("error");
     });
